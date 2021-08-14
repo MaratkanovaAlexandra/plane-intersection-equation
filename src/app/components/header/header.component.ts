7e14d83006f2860f7bcd099f95b269c624e1c4db
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-08-14 21:42:15 +0400

    feat: add points variable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  point = 100
   constructor() { }
 
   ngOnInit(): void {
