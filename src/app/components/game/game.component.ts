0f2f2cf3088e3b5439cefda761c7fe95cd576820
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-09-13 15:33:14 +0400

    feat: add game hints

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  firstPlane = "A1x + B1y + C1z + D1 = 0";
+  secondPlane = "A2x + B2y + C2z + D2 = 0";
+  hints = 1;
+  height = `${100/this.hints}%`;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getHint() {
+    switch(this.hints) {
+      case 1:
+        this.hints += 0.5;
+        break;
+      case 1.5:
+        this.hints += 1.5;
+        break;
+      case 3:
+        this.hints = 0.;
+        return;
+    }
+    this.height = `${100/this.hints}%`;
   }
 
 }
